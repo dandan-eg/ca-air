82479b3f4bd29e4449ea076cb2b1489eb08e2ec8
dandan-eg <egalledev@gmail.com>
2024-11-25 20:21:21 +0100

    air04.exs OK

---
+++ air04.exs
@@ -0,0 +1,31 @@
+# Un à la fois
+defmodule Exercice do
+  def remove_duplicate_ajdacent(<<>>), do: <<>>
+  def remove_duplicate_ajdacent(<<_char>> = string), do: string
+
+  def remove_duplicate_ajdacent(<<char, remaining::binary>>) do
+    do_remove_duplicate_adjacent(char, remaining, <<>>)
+  end
+
+  defp do_remove_duplicate_adjacent(current, <<current, remaining::binary>>, acc) do
+    do_remove_duplicate_adjacent(current, remaining, acc)
+  end
+
+  defp do_remove_duplicate_adjacent(current, <<next, remaining::binary>>, acc) do
+    do_remove_duplicate_adjacent(next, remaining, <<acc::binary, current>>)
+  end
+
+  defp do_remove_duplicate_adjacent(current, <<>>, acc) do
+    <<acc::binary, current>>
+  end
+end
+
+case System.argv() do
+  [string] ->
+    string
+    |> Exercice.remove_duplicate_ajdacent()
+    |> IO.puts()
+
+  _bad_args ->
+    IO.puts("usage: elixir air04.exs <string>")
+end
