6d2d2cc774e138a7d744e893af807b12284d47b8
dandan-eg <egalledev@gmail.com>
2024-11-25 20:20:45 +0100

    air03.exs OK

---
+++ air03.exs
@@ -0,0 +1,35 @@
+# Concat
+defmodule Exercice do
+  def concat(list, delimiter) do
+    do_concat(list, delimiter, <<>>)
+  end
+
+  defp do_concat([head], _delimiter, acc) do
+    <<acc::binary, head::bitstring>>
+  end
+
+  defp do_concat([head | tail], delimiter, acc) do
+    do_concat(
+      tail,
+      delimiter,
+      <<acc::binary, head::bitstring, delimiter::bitstring>>
+    )
+  end
+
+  defp do_concat([], _delimiter, acc), do: acc
+end
+
+case System.argv() do
+  [] ->
+    IO.puts("usage: elixir air03.exs <args..> <delemiter>")
+
+  [_] ->
+    IO.puts("usage: elixir air03.exs <args..> <delemiter>")
+
+  args ->
+    {delimiter, list} = List.pop_at(args, -1)
+
+    list
+    |> Exercice.concat(delimiter)
+    |> IO.inspect()
+end
