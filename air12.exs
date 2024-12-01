a4b4f6d91923b8d85fe41b162591c1b6a2d5e4d6
dandan-eg <egalledev@gmail.com>
2024-12-01 20:29:23 +0100

    comment benchmark

--- air12.exs
+++ air12.exs
@@ -16,22 +16,58 @@ defmodule Exercice do
       partition(tail, pivot, left, [head | right])
     end
   end
-end
 
-defmodule Benchmark do
-  def measure(context, fun) do
-    {time_in_microseconds, result} = :timer.tc(fun)
-    IO.puts("#{context} took #{time_in_microseconds / 100_000} seconds")
-    result
+  defp parse_args([]), do: {:error, :no_args}
+
+  defp parse_args(args) do
+    do_parse_args(args, [])
+  end
+
+  defp do_parse_args([head | tail], acc) do
+    case Integer.parse(head) do
+      {num, _rest} -> do_parse_args(tail, [num | acc])
+      :error -> {:error, {:nan, head}}
+    end
+  end
+
+  defp do_parse_args([], acc), do: {:ok, Enum.reverse(acc)}
+
+  def run do
+    System.argv()
+    |> parse_args()
+    |> case do
+      {:ok, numbers} ->
+        numbers
+        |> quicksort()
+        |> IO.inspect()
+
+      {:error, :no_args} ->
+        IO.puts("usage: air12.exs <numbers...>")
+        System.halt()
+
+      {:error, {:nan, arg}} ->
+        IO.puts("'#{arg}' is not a valid numbers")
+        System.halt()
+    end
   end
 end
 
-numbers = Enum.shuffle(for i <- 1..5_000_000, do: i)
+Exercice.run()
 
-Benchmark.measure("Exercice.quicksort", fn ->
-  Exercice.quicksort(numbers)
-end)
-
-Benchmark.measure("Enum.sort", fn ->
-  Enum.sort(numbers)
-end)
+# defmodule Benchmark do
+#   def measure(context, fun) do
+#     {time_in_microseconds, result} = :timer.tc(fun)
+#     IO.puts("#{context} took #{time_in_microseconds / 100_000} seconds")
+#     result
+#   end
+# end
+#
+# numbers = Enum.shuffle(for i <- 1..5_000_000, do: i)
+#
+# Benchmark.measure("Exercice.quicksort", fn ->
+#   Exercice.quicksort(numbers)
+# end)
+#
+# Benchmark.measure("Enum.sort", fn ->
+#   Enum.sort(numbers)
+# end)
