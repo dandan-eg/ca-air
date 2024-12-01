5606e4c66f573dfa2e2bd70db2786fbf813e37df
dandan-eg <egalledev@gmail.com>
2024-12-01 16:37:45 +0100

    reformat code

--- air08.exs
+++ air08.exs
@@ -17,7 +17,6 @@ defmodule Exercice do
         with {:ok, first_numbers} <- parse_args(first),
              {:ok, second_numbers} <- parse_args(second),
              :ok <- validate_sorted(first_numbers, second_numbers) do
-
           merged = merge(first_numbers, second_numbers)
           IO.inspect(merged)
         else
@@ -27,7 +26,6 @@ defmodule Exercice do
           {:error, {:nan, invalid_arg}} ->
             IO.puts("'#{invalid_arg}' is not a valid number.")
         end
-
     end
   end
 
@@ -44,20 +42,16 @@ defmodule Exercice do
 
   def sorted?([]), do: true
   def sorted?([_]), do: true
-  def sorted?([first | [second | _]]) when first > second, do: false
-
-  def sorted?([_ | tail]) do
-    sorted?(tail)
-  end
+  def sorted?([first, second | _]) when first > second, do: false
+  def sorted?([_ | tail]), do: sorted?(tail)
 
   def validate_sorted(first_numbers, second_numbers) do
-      if sorted?(first_numbers) && sorted?(second_numbers) do
-        :ok
-      else
-        {:error, :not_sorted}
-      end
+    if sorted?(first_numbers) && sorted?(second_numbers) do
+      :ok
+    else
+      {:error, :not_sorted}
+    end
   end
-
 
   def split_args(["fusion" | tail], acc), do: {:ok, Enum.reverse(acc), tail}
   def split_args([arg | tail], acc), do: split_args(tail, [arg | acc])
@@ -67,12 +61,11 @@ defmodule Exercice do
     do_merge(first_list, second_list, [])
   end
 
-
   defp do_merge([head | tail], [], acc), do: do_merge(tail, [], [head | acc])
   defp do_merge([], [head | tail], acc), do: do_merge(tail, [], [head | acc])
   defp do_merge([], [], acc), do: Enum.reverse(acc)
 
-  defp do_merge( first_numbers, second_numbers, acc) do
+  defp do_merge(first_numbers, second_numbers, acc) do
     first_head = hd(first_numbers)
     second_head = hd(second_numbers)
 
